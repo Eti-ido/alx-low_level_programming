9a3e39c650a0b530e1343d335537828520c3d52d
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-13 15:05:44 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+# include <unistd.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	result(1);
+}
