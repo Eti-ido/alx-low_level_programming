f638959d04aec3dd3175ab460621ea9ee6fe7eb6
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-13 15:10:02 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-	result(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }
