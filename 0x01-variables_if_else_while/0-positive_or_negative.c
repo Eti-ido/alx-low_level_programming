5b4a38e39ba98d75c15391a26a8a8d294a72c2b2
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-14 12:59:22 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,25 +9,24 @@
  *
  * Result - Always 0 (Success)
  */
-int main(void)
+void main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("value of n: ");
+	printf("n: ");
 	scanf("%d", &n);
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("n is zero\n");
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("n is negative\n");
+		printf("%d is negative\n", n);
 	}
-	return (0);
 }
