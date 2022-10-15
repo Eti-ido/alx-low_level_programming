bf6ec3e734756d57290e6c46c8d3a59eb017f901
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-15 04:01:58 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Result - Always 0 (Success)
+ *main - Entry point
+ *Description - "prints the size of various types"
+ *Return: Always 0 (Success)
  */
 
 int main(void)
-	/* prints the size of various types */
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
