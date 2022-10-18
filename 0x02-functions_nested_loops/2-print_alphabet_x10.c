2a006f8237af9cc8535b3af7be4a9ddf1dfe2a82
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-18 09:57:44 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int n = 0;
+	char alpha = 'a';
 
 	while (n < 10)
 	{
-		_putchar('abcdefghijklmnopqrstuvwxyz\n');
-		n++
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}	
+	
+		_putchar('\n');
+		n++;
 	}
-	_putchar('\n');
 }
