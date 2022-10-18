72d1f31bcebb51132dd1d5d4d176c7536a071f79
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-18 06:30:50 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - prints _putchar
@@ -8,7 +9,7 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	write(2, "_putchar\n", 10);
 
 	return (0);
 }
