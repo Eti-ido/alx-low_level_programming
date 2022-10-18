221e6452be948e555b0a9ae2ef24a1c9544653f0
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-18 10:02:44 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  *
  * Return: void
  */
@@ -15,8 +15,8 @@ void print_alphabet_x10(void)
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
-		}	
-	
+		}
+
 		_putchar('\n');
 		n++;
 	}
