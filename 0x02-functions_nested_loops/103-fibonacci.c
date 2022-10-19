aeb5c26424bee4c0ad9411fc2843a3c84ab280f0
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-19 13:00:19 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of the even numbers of Fibonacci
+ * Return: Nothing
+ */
+int main(void)
+{
+	unsigned long numb, a, b, c, sum;
+
+	a = sum = 0;
+	b = 1;
+
+	for (numb = 0; numb <= 50; numb++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c / 2 == 0 && c < 4000000)
+		{
+			sum += c;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
