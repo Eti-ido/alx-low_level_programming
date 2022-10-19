96c0af297ddf91db39d81613c2ac51838ec3c519
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-19 13:33:05 -0700

    Fixed Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,47 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_times_table - prints the n times table
- * @n: The number to be computed
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int d, e, f;
+	int i, j, res;
 
-	if (n <= 14 && n >= 0)
+	if (!(n > 15 || n < 0))
 	{
-		for (d = 0; d <= n; d++)
+		for (i = 0; i <= n; i++)
 		{
-			for (e = 0; e <= n; e++)
+			for (j = 0; j <= n; j++)
 			{
-				f = d * e;
-				if (d == 0)
-				{
-					_putchar(f + '0'); }
-				else if (f < 10 && d != 0)
+				res = (i * j);
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(f + '0'); }
-				else if (f >= 10 && f < 100)
+				}
+				if (res < 10 && j != 0)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((f / 10) + '0');
-					_putchar((f % 10) + '0'); }
-				else if (f >= 100)
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
 				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar((f / 100) + '0');
-					_putchar(((f / 10) % 10) + '0');
-					_putchar((f % 10) + '0'); }
-			} _putchar('\n');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
 	}
 }
