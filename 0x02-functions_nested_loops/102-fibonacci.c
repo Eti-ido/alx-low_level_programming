e40b66611687a620a30441cf3b2ca91f1b16938f
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-19 12:40:23 -0700

    Task 12 again

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	unsigned long numb, a, b, c;
 
-	a = 1;
-	b = 2;
-	for (numb = 0; numb <= 50; numb++)
+	a = 0;
+	b = 1;
+	for (numb = 0; numb < 50; numb++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
 		printf("%lu", c);
-		if (numb == 50)
+		if (numb == 49)
 			putchar('\n');
 		else
 		{
