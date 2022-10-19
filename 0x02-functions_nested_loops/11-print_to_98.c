82e4242249a5de9d7bd7c0e30f529ca20b394d45
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-19 06:52:55 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,15 +12,14 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = 0; n < 98; n++)
-		{
+		for (n = n; n < 98; n++)
 			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
+		printf("%d\n", 98);
+	}
 	else
 	{
-		for (n = 0; n > 98; n--)
+		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-			printf("%d\n", 98);
+		printf("%d\n", 98);
 	}
 }
