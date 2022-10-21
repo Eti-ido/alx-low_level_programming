70044fb8d30b03dbcb30288122d41e9b05a534aa
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-21 05:33:13 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,19 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	if (n != 0)
+	{
+		_putchar(n / 10);
+		_putchar((n % 10) + '0');
+	}
+
+	else if (n == 0)
+		putchar('0');
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
 }
