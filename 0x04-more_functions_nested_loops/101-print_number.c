1d9b81a9799b6643edbb2c0dc3c228c824075105
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-21 01:24:18 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * print_number -prints an integer
+ * @n: The integer to be printed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	while (n <= 0)
+	{
+		_putchar ((n % 10) = '0')
+		n--;
+	}
+	while (n >=)
+	{
+		_putchar((n % 10) + '0');
+		n++;
+	}
+}
