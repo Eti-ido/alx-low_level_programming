f4efaf9610bba2e3a8facdb5e5dd7d22c4e74430
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-25 09:08:11 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str != '\0')
-		i++;
+	for (; str[i] != '\0'; i++)
 	{
-		if (i % 2 = 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
 		else
+		{
 			continue;
+		}
 	}
 	_putchar('\n');
 }
