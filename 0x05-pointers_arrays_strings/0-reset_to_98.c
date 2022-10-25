a1ac23d5ac4ef57679cc95e23d995a3dc789ae44
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-25 05:22:14 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as param
+ * @n: pointer variable
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int r = 402;
+	int *n = &r;
+
+	*n = 98;
+}
