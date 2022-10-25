a62d345fa21ecdb96832e5001b74a608d55f59f1
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-25 09:29:38 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,13 @@ void puts_half(char *str)
 {
 	int i = 0;
 
-	while (str != 0)
+	while (*str != 0)
 	{
 		i++;
 		str++;
 	}
 	str -= (i / 2);
-	while (str != 0)
+	while (*str != 0)
 	{
 		_putchar(str);
 		str++;
