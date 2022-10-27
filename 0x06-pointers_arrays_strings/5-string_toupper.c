d7da41b21d22ae4ab576f90358665ebdea9cbfae
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-27 09:55:56 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * @f: pointer to character
+ * Return: character
+ */
+char *string_toupper(char *f)
+{
+	int i = 0;
+
+	while (f[i] != '\0')
+	{
+		if (f[i] >= 'a' && f[i] <= 'z')
+		{
+			f[i] = f[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
