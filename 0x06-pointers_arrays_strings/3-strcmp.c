03a601a69c18612cb3e9212193c7dacd69cec9e2
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-27 08:54:55 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else
+			return (0);
+	i++;
+	}
+	return (0);
+}
