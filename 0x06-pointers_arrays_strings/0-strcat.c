38cbae9279d015ab46833d1f9aed84432b179089
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-27 05:13:14 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination of the string
+ * @src: source of the string
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while ((src[i] = '\0'))
+	i++;
+	dest[i] = src[i];
+
+	return (dest);
+}
