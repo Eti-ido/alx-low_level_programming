70f4a23fafe87a2fa8a030513c5ce890fa80db33
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-27 07:05:17 -0700

    Task 1 fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - concatenates two strings
  * @dest: string destination
  * @src: source of the string
- * @n: string length
+ * @n: number of char to be copied
  * Return: destination of the string
  */
 char *_strncat(char *dest, char *src, int n)
@@ -12,16 +12,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while ((dest[j] != '\0'))
+	while (dest[j] != '\0')
 	j++;
 
-	while ((src[i] != '\0'))
+	while (i < n && src[i] != '\0')
 	{
-		dest[i + j] = src[i];
-		i++;
+		dest[j] = src[i];
 		j++;
+		i++;
 	}
-	dest[i + j] = '\0';
+	dest[j] = '\0';
 
 	return (dest);
 }
