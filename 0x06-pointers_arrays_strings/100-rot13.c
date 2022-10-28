1536d07402ecf0fcc2369e26c6f7887ce06e1c57
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-10-28 06:59:59 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @k: input
+ * Return: character
+ */
+char *rot13(char *k)
+{
+	int i = 0;
+	int j;
+	char let[] = "ABCDEFGHIJKLMabcdefghijklm";
+	char rot[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+
+	for (i = 0; k[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 'z' || j <= 'Z'; j++)
+		{
+			if (k[i] == let[i])
+			{
+				k[i] = rot[i];
+			}
+		}
+	}
+
+	return (k);
+}
