10123f5badb4cdd572f1a7dd0c8d5b179a149447
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-11-01 12:16:49 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to the searched
+ * @c: character to be located
+ * Return: first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < n; i++)
+		if (s[i] != '\0')
+		{
+			s[i] = c
+		}
+		else
+		{
+			s[i] = '\0'
+		}
+
+	return (c);
+}
