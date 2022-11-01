6aca2f3fd4b2022be8abe6752b799564551c9d40
Eti-ido Akpaettim <etiidoakpaettim@gmail.com>
2022-11-01 01:00:54 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
+	unsigned int j;
 
 	for (i = 0; i < n; i++)
 		if (src[i] != '\0')
-			src[i] = dest[i];
+			src[i] = dest[j];
 
 	return (dest);
 }
